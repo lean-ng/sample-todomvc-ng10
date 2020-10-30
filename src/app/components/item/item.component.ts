a246133ee5a958a2627e541c477988cc9c9bd00a
Michael Alt <mail@lean-stack.de>
2020-10-30 21:59:24 +0100

    Add template and input prop.

--- src/app/components/item/item.component.ts
+++ src/app/components/item/item.component.ts
@@ -1,10 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Todo } from 'src/app/models/todo';
 
 @Component({
   selector: 'todo-item',
   templateUrl: './item.component.html'
 })
 export class ItemComponent implements OnInit {
+
+  @Input()
+  todo: Todo | undefined;
 
   constructor() { }
 
