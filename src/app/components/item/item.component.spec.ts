8f423a8c335ac81e7be1a2901c693fa3a6e81ad7
Michael Alt <mail@lean-stack.de>
2020-10-30 22:19:22 +0100

    Display a todo item.

--- src/app/components/item/item.component.spec.ts
+++ src/app/components/item/item.component.spec.ts
@@ -1,6 +1,14 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { Todo } from 'src/app/models/todo';
 
 import { ItemComponent } from './item.component';
+
+const mockTitle = 'Unit Tests';
+const createMock = (title = mockTitle, completed = false): Todo => {
+  const todo: Todo = { id: 17, title, completed };
+  return Object.freeze(todo);
+};
 
 describe('ItemComponent', () => {
   let component: ItemComponent;
@@ -16,10 +24,35 @@ describe('ItemComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(ItemComponent);
     component = fixture.componentInstance;
+    component.todo = createMock();
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should render the todo title', () => {
+    const labelEL: HTMLLabelElement = fixture.nativeElement.querySelector('label');
+    expect(labelEL.textContent).toBe(mockTitle);
+  });
+
+  it('should set the checked state of the toggle checkbox', () => {
+    const toggleEL: HTMLInputElement = fixture.nativeElement.querySelector('.toggle');
+    expect(toggleEL.checked).toBeFalse();
+
+    component.todo = createMock(mockTitle, true);
+    fixture.detectChanges();
+    expect(toggleEL.checked).toBeTrue();
+  });
+
+  it('should apply the completed class whether checked', () => {
+    const liEL: HTMLLIElement = fixture.nativeElement.querySelector('li');
+    expect(liEL.classList.contains('completed')).toBeFalse();
+
+    component.todo = createMock(mockTitle, true);
+    fixture.detectChanges();
+    expect(liEL.classList.contains('completed')).toBeTrue();
+  });
+
 });
